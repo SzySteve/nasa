ccda689a6cfd992960a3561dbcdc1d5e574aff7e
Steve Szyszkiewicz <sszyszkiewicz@monetate.com>
2017-04-21 10:20:37 -0400

    add requirements

--- setup.py
+++ setup.py
@@ -10,11 +10,10 @@ with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
 requirements = [
-    # TODO: put package requirements here
+    'requests==2.13.0'
 ]
 
 test_requirements = [
-    # TODO: put package test requirements here
 ]
 
 setup(
